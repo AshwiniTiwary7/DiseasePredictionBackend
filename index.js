56a1a631914a4f26d720f3e2f17a18d190409144
Ashwini Tiwary <152381380+AshwiniTiwary7@users.noreply.github.com>
2024-03-13 01:46:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const cors = require('cors');
 const route = require('./Routes/Route');
 
 require('dotenv').config();
-app.use(cors(
-    {
-    origin: ["https://disease-prediction-backend.vercel.app"],
-    methods: ["POST" , "GET", "PUT", "DELETE"],
-    credentials: true
-    }));
+app.use(cors());
 app.use(express.json());
 app.use('/medLife/v1',route);
 
